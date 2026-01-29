001b567142932e5114da64425f54783950a378bc
Dixen-Naxos <armanddailly@gmail.com>
2026-01-29 12:07:57 +0100

    Optimize 3 conversion

--- src/roman_converter.py
+++ src/roman_converter.py
@@ -1,8 +1,2 @@
 def int_to_roman(number_to_convert: int) -> str:
-    if number_to_convert == 2:
-        return "II"
-
-    if number_to_convert == 3:
-        return "III"
-    
-    return "I"
+    return "I" * number_to_convert
