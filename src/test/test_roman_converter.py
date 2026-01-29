38149a302214e908d87d3f36370b5007223b89ab
Dixen-Naxos <armanddailly@gmail.com>
2026-01-29 12:19:12 +0100

    Add test cases

--- src/test/test_roman_converter.py
+++ src/test/test_roman_converter.py
@@ -4,6 +4,11 @@ from src.roman_converter import int_to_roman
 
 
 class TestRomanConverter:
-    @pytest.mark.parametrize("number_to_convert, expected", [[1, "I"], [2, "II"], [3, "III"], [5, "V"], [10, "X"]])
+    @pytest.mark.parametrize("number_to_convert, expected",
+                             [[1, "I"], [2, "II"], [3, "III"],
+                              [5, "V"], [6, "VI"], [7, "VII"],
+                              [8, "VIII"], [10, "X"], [11, "XI"],
+                              [12, "XII"], [13, "XIII"], [15, "XV"]
+                              ])
     def test_roman_to_int_for_number_one(self, number_to_convert, expected):
         assert int_to_roman(number_to_convert) == expected
