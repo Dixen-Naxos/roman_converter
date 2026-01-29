c98713eb6aa3faab81209fe45b5e9993ca232090
Dixen-Naxos <armanddailly@gmail.com>
2026-01-29 12:46:03 +0100

    Add conversions

--- src/roman_converter.py
+++ src/roman_converter.py
@@ -1,18 +1,25 @@
+ROMAN_NUMBER_ASSOCIATION: list[list] = [
+    [100, "C"],
+    [90, "XC"],
+    [50, "L"],
+    [40, "XL"],
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"],
+]
 def int_to_roman(number_to_convert: int) -> str:
     roman_accumulator: str = ""
     counter: int = number_to_convert
 
+
     while counter > 0:
-        if counter >= 10:
-            roman_accumulator += "X"
-            counter -= 10
-
-        if counter >= 5:
-            roman_accumulator += "V"
-            counter -= 5
-
-        if counter >= 1:
-            roman_accumulator += "I"
-            counter -= 1
+        for roman_number_couple in ROMAN_NUMBER_ASSOCIATION:
+            value, symbol = roman_number_couple
+            if counter >= value:
+                roman_accumulator += symbol
+                counter -= value
+                break
 
     return roman_accumulator
--- src/test/test_roman_converter.py
+++ src/test/test_roman_converter.py
@@ -5,10 +5,10 @@ from src.roman_converter import int_to_roman
 
 class TestRomanConverter:
     @pytest.mark.parametrize("number_to_convert, expected",
-                             [[1, "I"], [2, "II"], [3, "III"],
+                             [[1, "I"], [2, "II"], [3, "III"], [4, "IV"],
                               [5, "V"], [6, "VI"], [7, "VII"],
-                              [8, "VIII"], [10, "X"], [11, "XI"],
-                              [12, "XII"], [13, "XIII"], [15, "XV"]
-                              ])
+                              [8, "VIII"], [9, "IX"],[10, "X"], [11, "XI"],
+                              [12, "XII"], [13, "XIII"], [15, "XV"],
+                              [20, "XX"], [40, "XL"], [60, "LX"], [90, "XC"],[100, "C"],])
     def test_roman_to_int_for_number_one(self, number_to_convert, expected):
         assert int_to_roman(number_to_convert) == expected
