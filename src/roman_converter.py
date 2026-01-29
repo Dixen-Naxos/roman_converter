e41393385056436d98ecd7ee047547ec73b3ddda
Dixen-Naxos <armanddailly@gmail.com>
2026-01-29 12:15:33 +0100

    Add 5 conversion

--- src/roman_converter.py
+++ src/roman_converter.py
@@ -1,2 +1,9 @@
 def int_to_roman(number_to_convert: int) -> str:
-    return "I" * number_to_convert
+    roman_accumulator: str = ""
+    for i in range(1, number_to_convert + 1):
+        roman_accumulator += "I"
+        if i % 5 == 0:
+            print(roman_accumulator)
+            roman_accumulator = roman_accumulator.replace("IIIII", "V")
+
+    return roman_accumulator
--- src/test/test_roman_converter.py
+++ src/test/test_roman_converter.py
@@ -4,6 +4,6 @@ from src.roman_converter import int_to_roman
 
 
 class TestRomanConverter:
-    @pytest.mark.parametrize("number_to_convert, expected", [[1, "I"], [2, "II"], [3, "III"], [4, "IV"]])
+    @pytest.mark.parametrize("number_to_convert, expected", [[1, "I"], [2, "II"], [3, "III"], [5, "V"]])
     def test_roman_to_int_for_number_one(self, number_to_convert, expected):
         assert int_to_roman(number_to_convert) == expected
